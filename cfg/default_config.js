38a22fb9c33f14412deb42f98c637e4640b09783
Unknown <daniel.gruve@gmail.com>
2019-02-24 13:42:26 +0100

    add config files

---
+++ cfg/default_config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.discord = {};
+
+/*******************************************
+** DISCORD setup
+*******************************************/
+
+config.discord.token = ''; // discord bot token
+
+
+module.exports = config;
