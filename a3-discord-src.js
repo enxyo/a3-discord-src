02210d0e24419166c40be175041d997e99dbce14
Unknown <daniel.gruve@gmail.com>
2019-02-24 13:58:22 +0100

    added basic bot framework

---
+++ a3-discord-src.js
@@ -0,0 +1,35 @@
+const config = require('./cfg/config');
+
+/*******************************************
+** DISCORD setup
+*******************************************/
+
+const discord = require("discord.js");
+const client = new discord.Client();
+const token = config.discord.token;
+
+// Set the prefix
+const prefix = ".";
+
+/*******************************************
+** DISCORD
+*******************************************/
+
+client.on("ready", () => {
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+    client.user.setPresence({ game: { name: 'mortar incoming', type: 0 } });
+});
+
+client.on("message", (message) => {
+	if(message.author.bot) return;
+	if(message.content.indexOf(prefix) !== 0) return;
+
+	const args = message.content.slice(prefix.length).trim().toLowerCase().split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+	if (command  === 'help') {
+		message.channel.send('**Dir ist nicht zu helfen**');
+	}
+});
+
+client.login(token);
